aa53ebb9d4273d28b5b7108683be69bd889f2f10
Anthony Chu <antchu@microsoft.com>
2020-07-13 12:45:08 -0700

    Use DefaultAzureCredential
    Requires pre-release packages for now

--- ListResourceGroups/__init__.py
+++ ListResourceGroups/__init__.py
@@ -3,23 +3,17 @@ import logging
 import os
 
 import azure.functions as func
-from azure.common.credentials import get_azure_cli_credentials
 from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
-from msrestazure.azure_active_directory import MSIAuthentication
+from azure.identity import DefaultAzureCredential
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    if "MSI_ENDPOINT" in os.environ:
-        credentials = MSIAuthentication()
-    else:
-        credentials, *_ = get_azure_cli_credentials()
-
+    credentials = DefaultAzureCredential()
     subscription_client = SubscriptionClient(credentials)
     subscription = next(subscription_client.subscriptions.list())
     subscription_id = subscription.subscription_id
 
     client = ResourceManagementClient(credentials, subscription_id)
-
     resource_groups = [g.name for g in client.resource_groups.list()]
 
     return func.HttpResponse(json.dumps(resource_groups), mimetype="application/json")
